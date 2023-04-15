740b1c6aabeeed76609253fe65c139455b82677d
Garvit Batra <batra.garvit28@gmail.com>
2023-04-16 00:01:42 +0530

    search optimized and owner pic feature

--- routes/Article.js
+++ routes/Article.js
@@ -3,31 +3,38 @@ const router = express.Router();
 const redirectLogin = require("./redirects/redirectLogin");
 const lost = require("../models/Lost");
 const found = require("../models/Found");
+const img = require("../models/Img");
 router.get("/article/:topic", redirectLogin, function (req, res) {
   const post = req.params.topic;
   lost.findOne({ _id: post }, function (err, result) {
     if (result) {
-      return res.render("Article", {
-        title: result.title,
-        content: result.description,
-        owner: result.username,
-        image: result,
-        date: result.date,
-        time: result.time,
-        category:result.category,
+      img.findOne({ email: result.email }, function (err, pic) {
+        return res.render("Article", {
+          title: result.title,
+          content: result.description,
+          owner: result.username,
+          image: result,
+          date: result.date,
+          time: result.time,
+          category: result.category,
+          image1: pic,
+        });
       });
     }
   });
   found.findOne({ _id: post }, function (err, result) {
     if (result) {
-      return res.render("Article", {
-        title: result.title,
-        content: result.description,
-        owner: result.username,
-        image: result,
-        date: result.date,
-        time: result.time,
-        category:result.category,
+      img.findOne({ email: result.email }, function (err, pic) {
+        return res.render("Article", {
+          title: result.title,
+          content: result.description,
+          owner: result.username,
+          image: result,
+          date: result.date,
+          time: result.time,
+          category: result.category,
+          image1: pic,
+        });
       });
     }
   });
